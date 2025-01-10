22a2e841e95027ccab4fdcb6801df7fb7e60a745
Raghav Sood <r@raghavsood.com>
2025-01-11 02:21:43 +0800

    flake: init

---
+++ flake.nix
@@ -0,0 +1,43 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    systems.url = "github:nix-systems/default";
+    devenv.url = "github:cachix/devenv/1e4701fb1f51f8e6fe3b0318fc2b80aed0761914";
+  };
+
+  nixConfig = {
+    extra-trusted-public-keys = "devenv.cachix.org-1:w1cLUi8dv3hnoSPGAuibQv+f9TZLr6cv/Hm9XgU50cw=";
+    extra-substituters = "https://devenv.cachix.org";
+  };
+
+  outputs = { self, nixpkgs, devenv, systems, ... } @ inputs:
+    let
+      forEachSystem = nixpkgs.lib.genAttrs (import systems);
+    in
+    {
+      devShells = forEachSystem
+        (system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+          in
+          {
+            default = devenv.lib.mkShell {
+              inherit inputs pkgs;
+              modules = [
+                {
+                  packages = with pkgs; [ 
+                    ruby_3_4
+
+                    # Ensure we use openssl 3
+                    openssl_3 
+                  ];
+
+                  enterShell = ''
+                    export PATH=$HOME/.gem/ruby/3.4.0/bin:$PATH
+                  '';
+                }
+              ];
+            };
+          });
+    };
+}
